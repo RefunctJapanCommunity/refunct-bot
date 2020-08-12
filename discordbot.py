cdbc0963f236c74408594267a5da2bff5c949727
gunimus <gunimus@overspec.net>
2020-08-12 23:28:05 +0900

    Fix send split

--- discordbot.py
+++ discordbot.py
@@ -31,13 +31,35 @@ async def c(ctx):
 
 @bot.command()
 async def ranking(ctx, arg):
-    if "雑談" in str(ctx.channel) or "記録確認所" in str(ctx.channel):
-        if "Refunct" in str(ctx.guild):
-            send_list = refunct.get_ranking(arg)
-        else:
-            send_list = speedrun.get_ranking(arg)
+    if "Refunct" in str(ctx.guild):
+        send_list = refunct.get_ranking(arg)
         send_str = '\n'.join(send_list)
         await ctx.send(send_str)
+    elif "記録確認所" in str(ctx.channel):
+        user, user_id = speedrun.get_user(arg)
+        send_str = '\n'.join(user)
+        await ctx.send(send_str)
+        user_data = speedrun.get_user_data(user_id)
+        send_list = []
+        game_id = ''
+        count = 0
+        for data in user_data:
+            previous_id = game_id
+            game_id = data.get('run').get('game')
+            if previous_id != game_id:
+                if count % 3 == 0 and count > 2:
+                    send_str = '\n'.join(send_list)
+                    await ctx.send(send_str)
+                    send_list.clear()
+                count = count + 1
+                send_list.append('\n' + speedrun.get_title(game_id))
+            send_list.append(speedrun.get_records(data))
+        if len(send_list) == 1:
+            send_list.append('\n' + 'records is not found')
+        send_str = '\n'.join(send_list)
+        await ctx.send(send_str)
+        send_str = '\n'.join(user)
+        await ctx.send(f'{send_str} record is End')
 
 @bot.command()
 async def tech(ctx, arg):
--- speedrun.py
+++ speedrun.py
@@ -3,8 +3,9 @@ import datetime
 import common
 
 prefix_url = 'https://www.speedrun.com/api/v1'
+game_id = ''
 
-def get_ranking(user_name):
+def get_user(user_name):
     send_list = []
     user_id = get_user_id(user_name)
     if user_id == '':
@@ -12,8 +13,11 @@ def get_ranking(user_name):
         return send_list
     
     send_list.append('user name : ' + user_name)
-    user_data = get_user_data(user_id)
+    return send_list, user_id
 
+def get_ranking(user_data):
+    global game_id
+    send_list = []
     game_id = ''
     for data in user_data:
         previous_id = game_id
@@ -52,15 +56,18 @@ def get_records(data):
         if links.get('rel') == 'category':
             category_url = links.get('uri')
             category_data = requests.get(category_url).json()
-            category_name = category_data.get('data').get('name')
-            if len(run_values) != 0: 
-                for category_links in category_data.get('data').get('links'):
-                    if category_links.get('rel') == 'variables':
-                        variables_url = category_links.get('uri')
-                        variables_choices = requests.get(variables_url).json().get('data')[0].get('values').get('choices')
-                        for variables_keys in variables_choices.keys():
-                            for run_value in run_values.values():
-                                if run_value == variables_keys:
-                                    variables_name = variables_choices.get(variables_keys)
-                                    category_name = category_name + ' (' + variables_name + ')'
+            if category_data.get('data').get('type') == "per-game":
+                category_name = category_data.get('data').get('name')
+                if len(run_values) != 0: 
+                    for category_links in category_data.get('data').get('links'):
+                        if category_links.get('rel') == 'variables':
+                            variables_url = category_links.get('uri')
+                            variables_choices = requests.get(variables_url).json().get('data')[0].get('values').get('choices')
+                            for variables_keys in variables_choices.keys():
+                                for run_value in run_values.values():
+                                    if run_value == variables_keys:
+                                        variables_name = variables_choices.get(variables_keys)
+                                        category_name = category_name + ' (' + variables_name + ')'
+            else:
+                return ''
     return '  ' + category_name + '\n' + '    ' + place + ' (' + run_time + ')'
